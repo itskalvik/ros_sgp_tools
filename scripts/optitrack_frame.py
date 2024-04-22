739442d3b474d151c293fb679960c08e60dd25d1
itskalvik <kjakkala@uncc.edu>
2024-04-22 09:55:25 -0400

    Setup optitrack tf transforms

--- scripts/optitrack_frame.py
+++ scripts/optitrack_frame.py
@@ -1,5 +1,9 @@
 #! /usr/bin/env python3
+from tf.transformations import quaternion_matrix, quaternion_from_matrix
 from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import Odometry
+import message_filters
+import numpy as np
 import rospy
 import tf
 
@@ -21,27 +25,56 @@ class OptiTrackFrame:
         else:
             self.robot_index = 1
 
-        vrpn_sub = rospy.Subscriber('/vrpn_client_node/pose',
-                                    PoseStamped, 
-                                    self.callback)
-        
+        vrpn_sub = message_filters.Subscriber('/vrpn_client_node/pose', 
+                                               PoseStamped)
+        odom_sub = message_filters.Subscriber('/odom', Odometry)
+        ts = message_filters.ApproximateTimeSynchronizer([vrpn_sub, odom_sub], 
+                                                         10, 0.1, allow_headerless=True)
+        ts.registerCallback(self.callback)
+
         # Setup the frame broadcaster
         self.br = tf.TransformBroadcaster()
+        self.pos = (0., 0., 0.)
+        self.rot = (0., 0., 0., 1.)
 
+        rate = rospy.Rate(10)
         rospy.loginfo('OptiTrackFrame Publisher initialized')
-        rospy.spin()
 
-    def callback(self, msg): 
-        self.br.sendTransform((msg.pose.position.x,
-                               msg.pose.position.y,
-                               msg.pose.position.z),
-                              (msg.pose.orientation.x,
-                               msg.pose.orientation.y,
-                               msg.pose.orientation.z,
-                               msg.pose.orientation.w),
+        while not rospy.is_shutdown():
+            self.publish_frame()
+            rate.sleep()
+
+    def callback(self, vrpn_msg, odom_msg):
+        Twv = PoseStamped_2_mat(vrpn_msg)
+        Two = PoseStamped_2_mat(odom_msg.pose)
+
+        Tvw = T_inv(Twv)
+        Tvo = np.matmul(Tvw, Two)
+        q = quaternion_from_matrix(Tvo) 
+
+        self.pos = (Tvo[0,3], Tvo[1,3], Tvo[2,3])
+        self.rot = (q[0], q[1], q[2], q[3])
+
+    def publish_frame(self):
+        self.br.sendTransform(self.pos, self.rot,
                               rospy.Time.now(),
-                              "base_footprint", "optitrack")
+                              "odom", "optitrack")
         # transform from frame "optitrack" to frame "base_footprint"
+
+def PoseStamped_2_mat(p):
+    q = p.pose.orientation
+    pos = p.pose.position
+    T = quaternion_matrix([q.x,q.y,q.z,q.w])
+    T[:3,3] = np.array([pos.x,pos.y,pos.z])
+    return T
+
+def T_inv(T_in):
+    R_in = T_in[:3,:3]
+    t_in = T_in[:3,[-1]]
+    R_out = R_in.T
+    t_out = -np.matmul(R_out,t_in)
+    return np.vstack((np.hstack((R_out,t_out)),np.array([0, 0, 0, 1])))
+
 
 if __name__ == '__main__':
     try:
