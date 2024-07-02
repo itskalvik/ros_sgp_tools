2f4cecdc824b99b2e95e4d287f8e2b3a6070d4a2
itskalvik <kjakkala@uncc.edu>
2024-07-02 14:28:37 -0400

    Add guided mission and fix makefile

--- scripts/guided_mission.py
+++ scripts/guided_mission.py
@@ -1,52 +1,19 @@
 #! /usr/bin/env python3
 
-from ros_sgp_tools.srv import Waypoints
-from geometry_msgs.msg import Twist, PoseStamped
-from std_msgs.msg import Int32
+from mavros_msgs.msg import State
+from mavros_msgs.srv import SetMode, CommandBool
+from geographic_msgs.msg import GeoPoseStamped
+from sensor_msgs.msg import NavSatFix
 
 import rclpy
 from rclpy.node import Node
 
-from time import sleep
 import numpy as np
 
 class PathPlanner(Node):
 
-    def __init__(self, distance_tolerance=0.1,
-                  angle_tolerance=0.1,
-                  update_rate=30,):
-        
-        self.distance_tolerance = distance_tolerance
-        self.angle_tolerance = angle_tolerance
-
+    def __init__(self):
         super().__init__('path_planner')
-
-        self.ns = self.get_namespace()
-
-        # Setup current waypoint publisher
-        self.current_waypoint_publisher = self.create_publisher(Int32, 
-                                                                'current_waypoint', 
-                                                                10)
-        
-        # Serup waypoint service
-        self.waypoint_service = self.create_service(Waypoints,
-                                                    '/waypoints',
-                                                    self.waypoint_service_callback)
-      
-        self.current_waypoint_timer = self.create_timer(update_rate/100, self.publish_current_waypoint)
-
-
-        # Setup ROS Node
-        self.position = np.array([0, 0, 0])
-        self.control_cmd = Twist()
-        self.waypoints = [] 
-        self.current_waypoint = -1
-            
-
-        self.get_logger().info(self.ns+'Path Planner: initialized, waiting for waypoints')
-
-        # Keep alive until waypoints are received and then send vel commands at update rate
-        rclpy.spin(self)
 
         # Create QoS profiles
 
@@ -63,12 +30,12 @@ class PathPlanner(Node):
         self.vehicle_state_subscriber = self.create_subscription(
             State, '/mavros/state', self.vehicle_state_callback, STATE_QOS)
         self.vehicle_pose_subscriber = self.create_subscription(
-            PoseStamped, '/mavros/local_position/pose', 
-            self.vehicle_pose_callback, SENSOR_QOS)
+            NavSatFix, '/mavros/global_position/global', 
+            self.vehicle_position_callback, SENSOR_QOS)
         
         # Create publishers
         self.setpoint_position_publisher = self.create_publisher(
-            PoseStamped, '/mavros/setpoint_position/local', SENSOR_QOS)
+            GeoPoseStamped, '/mavros/setpoint_position/global', SENSOR_QOS)
 
         # Create service clients
         self.set_mode_client = self.create_client(
@@ -85,34 +52,19 @@ class PathPlanner(Node):
 
         # Initialize variables
         self.vehicle_state = State()
-        self.vehicle_pose = np.array([0., 0.])
+        self.vehicle_position = np.array([0., 0.])
         self.arm_request = CommandBool.Request()
         self.set_mode_request = SetMode.Request()
-        self.setpoint_position = PoseStamped()
+        self.setpoint_position = GeoPoseStamped()
         self.waypoint_id = 0
         self.last_request = self.get_clock().now().to_msg().sec
 
         # Create a timer to publish control commands
         self.timer = self.create_timer(0.1, self.timer_callback)
-        
-    def waypoint_service_callback(self, request, response):
-        waypoints = request.waypoints.waypoints
-        self.waypoints = []
-        for i in range(len(waypoints)):
-            self.waypoints.append([waypoints[i].x, waypoints[i].y])
-        self.get_logger().info(self.ns+'Path Planner: Waypoints received')
-        response.current_waypoint = self.current_waypoint
-        return response
 
-    
-    def publish_current_waypoint(self):
-        current_waypoint_msg = Int32()
-        current_waypoint_msg.data = self.current_waypoint
-        self.current_waypoint_publisher.publish(current_waypoint_msg)    
-
-    def at_waypoint(self, waypoint, tolerance=0.5):
+    def at_waypoint(self, waypoint, tolerance=0.000005):
         """Check if the vehicle is at the waypoint."""
-        dist = np.linalg.norm(self.vehicle_pose - np.array(waypoint))
+        dist = np.linalg.norm(self.vehicle_position - np.array(waypoint))
         if dist < tolerance:
             return True
         else:
@@ -122,10 +74,10 @@ class PathPlanner(Node):
         """Callback function for vehicle_state topic subscriber."""
         self.vehicle_state = vehicle_state
 
-    def vehicle_pose_callback(self, vehicle_pose):
-        """Callback function for vehicle_pose topic subscriber."""
-        self.vehicle_pose = np.array([vehicle_pose.pose.position.x, 
-                                      vehicle_pose.pose.position.y])
+    def vehicle_position_callback(self, position):
+        """Callback function for vehicle position topic subscriber."""
+        self.vehicle_position = np.array([position.latitude, 
+                                          position.longitude])
 
     def arm(self):
         """Send an arm command to the vehicle."""
@@ -146,12 +98,11 @@ class PathPlanner(Node):
             self.set_mode_client.call_async(self.set_mode_request)
             self.get_logger().info('Engage GUIDED mode command sent')
             
-    def set_waypoint(self, x, y, z=0.0):
+    def set_waypoint(self, latitude, longitude):
         """Set the next waypoint of the vehicle."""
         self.setpoint_position.header.stamp = self.get_clock().now().to_msg()
-        self.setpoint_position.pose.position.x = x
-        self.setpoint_position.pose.position.y = y
-        self.setpoint_position.pose.position.z = z
+        self.setpoint_position.pose.position.latitude = latitude
+        self.setpoint_position.pose.position.longitude = longitude
         self.setpoint_position_publisher.publish(self.setpoint_position)
 
     def timer_callback(self) -> None:
@@ -172,31 +123,30 @@ class PathPlanner(Node):
 
         # Set the position setpoint
         if self.waypoint_id == 0:
-            if self.at_waypoint([5., 0.]):
+            if self.at_waypoint([35.30684387683425, -80.7360063599907]):
                 self.waypoint_id = 1
                 self.get_logger().info("Waypoint 1 reached")
-            self.set_waypoint(5., 0.,)
+            self.set_waypoint(35.30684387683425, -80.7360063599907)
         if self.waypoint_id == 1:
-            if self.at_waypoint([5., 5.]):
+            if self.at_waypoint([35.30684275566786, -80.73612370299257]):
                 self.waypoint_id = 2
                 self.get_logger().info("Waypoint 2 reached")
-            self.set_waypoint(5., 5.)
+            self.set_waypoint(35.30684275566786, -80.73612370299257)
         if self.waypoint_id == 2:
-            if self.at_waypoint([0., 5.]):
+            if self.at_waypoint([35.30679876645213, -80.73623439122146]):
                 self.waypoint_id = 3
                 self.get_logger().info("Waypoint 3 reached")
-            self.set_waypoint(0., 5.)
+            self.set_waypoint(35.30679876645213, -80.73623439122146)
         if self.waypoint_id == 3:
-            if self.at_waypoint([0., 0.]):
+            if self.at_waypoint([35.30674267884529, -80.73600329951549]):
                 self.waypoint_id = 4
                 self.get_logger().info("Waypoint 4 reached")
                 self.disarm()
                 exit()
-            self.set_waypoint(0., 0.)
+            self.set_waypoint(35.30674267884529, -80.73600329951549)
 
 
 def main(args=None):
-    
     rclpy.init(args=args)
     path_planner = PathPlanner()
     rclpy.spin(path_planner)
