93b99621a477cdf9723a598e30ee8c11543e9f41
itskalvik <kjakkala@uncc.edu>
2024-07-04 15:41:56 -0400

    Update guided mission to use executors

--- scripts/guided_mission.py
+++ scripts/guided_mission.py
@@ -7,22 +7,24 @@ from sensor_msgs.msg import NavSatFix
 
 import rclpy
 from rclpy.node import Node
+from rclpy.executors import ExternalShutdownException
+from rclpy.executors import MultiThreadedExecutor
 
+from time import sleep
 import numpy as np
+
 
 class PathPlanner(Node):
 
     def __init__(self):
-        super().__init__('path_planner')
+        super().__init__('PathPlanner')
 
         # Create QoS profiles
-
         # STATE_QOS used for state topics, like ~/state, ~/mission/waypoints etc.
         STATE_QOS = rclpy.qos.QoSProfile(
             depth=10, 
             durability=rclpy.qos.QoSDurabilityPolicy.TRANSIENT_LOCAL
         )
-
         # SENSOR_QOS used for most of sensor streams
         SENSOR_QOS = rclpy.qos.qos_profile_sensor_data
 
@@ -38,14 +40,12 @@ class PathPlanner(Node):
             GeoPoseStamped, '/mavros/setpoint_position/global', SENSOR_QOS)
 
         # Create service clients
-        self.set_mode_client = self.create_client(
-            SetMode, '/mavros/set_mode')
+        self.set_mode_client = self.create_client(SetMode, '/mavros/set_mode')
         while not self.set_mode_client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('Set mode service not available, waiting again...')
         self.get_logger().info('Set mode service available')
 
-        self.arm_client = self.create_client(
-            CommandBool, '/mavros/cmd/arming')
+        self.arm_client = self.create_client(CommandBool, '/mavros/cmd/arming')
         while not self.arm_client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('Arming service not available, waiting again...')
         self.get_logger().info('Arming service available')
@@ -56,11 +56,6 @@ class PathPlanner(Node):
         self.arm_request = CommandBool.Request()
         self.set_mode_request = SetMode.Request()
         self.setpoint_position = GeoPoseStamped()
-        self.waypoint_id = 0
-        self.last_request = self.get_clock().now().to_msg().sec
-
-        # Create a timer to publish control commands
-        self.timer = self.create_timer(0.1, self.timer_callback)
 
     def at_waypoint(self, waypoint, tolerance=0.000005):
         """Check if the vehicle is at the waypoint."""
@@ -79,78 +74,128 @@ class PathPlanner(Node):
         self.vehicle_position = np.array([position.latitude, 
                                           position.longitude])
 
-    def arm(self):
-        """Send an arm command to the vehicle."""
-        self.arm_request.value = True
-        self.arm_client.call_async(self.arm_request)
-        self.get_logger().info('Arm command sent')
+    def arm(self, state=True, timeout=30):
+        """Arm/Disarm the vehicle"""
+        self.arm_request.value = state
+        if state:
+            str_state = 'Arm'
+        else:
+            str_state = 'Disarm'
+        
+        start_time = self.get_clock().now().to_msg().sec
+        last_request = start_time-6.0
+        while self.vehicle_state.armed != state:
+            # Send the command only once every 5 seconds
+            if self.get_clock().now().to_msg().sec - last_request < 5.0:
+                sleep(1)
+                continue
 
-    def disarm(self):
-        """Send a disarm command to the vehicle."""
-        self.arm_request.value = False
-        self.arm_client.call_async(self.arm_request)
-        self.get_logger().info('Disarm command sent')
+            self.arm_client.call_async(self.arm_request)
+            last_request = self.get_clock().now().to_msg().sec
 
-    def engage_guided_mode(self):
-        """Send a command to the vehicle to enter GUIDED mode."""
-        if self.set_mode_client.wait_for_service(timeout_sec=0.5):
-            self.set_mode_request.custom_mode = "GUIDED"
+            # Timeout
+            if self.vehicle_state.armed != state and \
+                last_request - start_time > timeout:
+               
+                self.get_logger().info(f'Timeout: Failed to {str_state}')
+                return False
+
+        return True
+
+    def engage_mode(self, mode="GUIDED", timeout=30):
+        """Set the vehicle mode"""
+        self.set_mode_request.custom_mode = mode
+        
+        start_time = self.get_clock().now().to_msg().sec
+        last_request = start_time-6.0
+        while not self.vehicle_state.guided:
+            # Send the command only once every 5 seconds
+            if self.get_clock().now().to_msg().sec - last_request < 5.0:
+                sleep(1)
+                continue
+
             self.set_mode_client.call_async(self.set_mode_request)
-            self.get_logger().info('Engage GUIDED mode command sent')
-            
-    def set_waypoint(self, latitude, longitude):
-        """Set the next waypoint of the vehicle."""
-        self.setpoint_position.header.stamp = self.get_clock().now().to_msg()
-        self.setpoint_position.pose.position.latitude = latitude
-        self.setpoint_position.pose.position.longitude = longitude
-        self.setpoint_position_publisher.publish(self.setpoint_position)
+            last_request = self.get_clock().now().to_msg().sec
 
-    def timer_callback(self) -> None:
-        """Callback function for the timer."""
+            # Timeout
+            if not self.vehicle_state.guided and \
+                last_request - start_time > timeout:
+               
+                self.get_logger().info(f'Timeout: Failed to engage {mode} mode')
+                return False
 
-        # Engage GUIDED mode
-        if self.vehicle_state.mode != "GUIDED" and \
-            (self.get_clock().now().to_msg().sec - self.last_request) > 5.0:
-            self.engage_guided_mode()
-            self.last_request = self.get_clock().now().to_msg().sec
+        return True
+    
+    def go2waypoint(self, waypoint, timeout=900):
+        """Go to waypoint (latitude, longitude) when in GUIDED mode and armed"""
+        self.setpoint_position.pose.position.latitude = waypoint[0]
+        self.setpoint_position.pose.position.longitude = waypoint[1]
 
-        # Arm the vehicle
-        if not self.vehicle_state.armed and \
-            self.vehicle_state.mode == "GUIDED" and \
-            (self.get_clock().now().to_msg().sec - self.last_request) > 5.0:
-            self.arm()
-            self.last_request = self.get_clock().now().to_msg().sec
+        start_time = self.get_clock().now().to_msg().sec
+        last_request = start_time-6.0
+        while not self.at_waypoint(waypoint):
+            # Send the command only once every 5 seconds
+            if self.get_clock().now().to_msg().sec - last_request < 5.0:
+                sleep(1)
+                continue
 
-        # Set the position setpoint
-        if self.waypoint_id == 0:
-            if self.at_waypoint([35.30684387683425, -80.7360063599907]):
-                self.waypoint_id = 1
-                self.get_logger().info("Waypoint 1 reached")
-            self.set_waypoint(35.30684387683425, -80.7360063599907)
-        if self.waypoint_id == 1:
-            if self.at_waypoint([35.30684275566786, -80.73612370299257]):
-                self.waypoint_id = 2
-                self.get_logger().info("Waypoint 2 reached")
-            self.set_waypoint(35.30684275566786, -80.73612370299257)
-        if self.waypoint_id == 2:
-            if self.at_waypoint([35.30679876645213, -80.73623439122146]):
-                self.waypoint_id = 3
-                self.get_logger().info("Waypoint 3 reached")
-            self.set_waypoint(35.30679876645213, -80.73623439122146)
-        if self.waypoint_id == 3:
-            if self.at_waypoint([35.30674267884529, -80.73600329951549]):
-                self.waypoint_id = 4
-                self.get_logger().info("Waypoint 4 reached")
-                self.disarm()
-                exit()
-            self.set_waypoint(35.30674267884529, -80.73600329951549)
+            self.setpoint_position.header.stamp = self.get_clock().now().to_msg()
+            self.setpoint_position_publisher.publish(self.setpoint_position)
+            last_request = self.get_clock().now().to_msg().sec
+
+            # Timeout
+            if not self.at_waypoint(waypoint) and \
+                last_request - start_time > timeout:
+               
+                self.get_logger().info(f'Timeout: Failed to go to waypoint: {waypoint[0]}, {waypoint[1]}')
+                return False
+
+        return True
+
+    def mission(self):
+        "GUIDED mission"
+
+        sleep(5) # Wait to get the state of the vehicle
+
+        if self.arm(True):
+            self.get_logger().info('Armed')
+
+        if self.engage_mode('GUIDED'):
+            self.get_logger().info('GUIDED mode Engaged')
+
+        if self.go2waypoint([35.30684387683425, -80.7360063599907]):
+            self.get_logger().info('Reached waypoint')
+
+        if self.go2waypoint([35.30684275566786, -80.73612370299257]):
+            self.get_logger().info('Reached waypoint')
+
+        if self.go2waypoint([35.30679876645213, -80.73623439122146]):
+            self.get_logger().info('Reached waypoint')
+
+        if self.go2waypoint([35.30674267884529, -80.73600329951549]):
+            self.get_logger().info('Reached waypoint')
+
+        if self.arm(False):
+            self.get_logger().info('Disarmed')
+
+        rclpy.shutdown()
 
 
 def main(args=None):
     rclpy.init(args=args)
-    path_planner = PathPlanner()
-    rclpy.spin(path_planner)
-    rclpy.shutdown()
+
+    try:
+        path_planner = PathPlanner()
+
+        executor = MultiThreadedExecutor(num_threads=4)
+        executor.add_node(path_planner)
+        executor.create_task(path_planner.mission)
+        executor.spin()
+
+    except KeyboardInterrupt:
+        pass
+    except ExternalShutdownException:
+        rclpy.shutdown()
     
 
 if __name__ == '__main__':
