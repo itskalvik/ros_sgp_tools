84fa3c5ee8cebceedc626376e29007354c32e9ea
itskalvik <kjakkala@uncc.edu>
2024-07-13 10:32:02 -0400

    Fix mavros namespace config

--- launch/single_robot.launch.py
+++ launch/single_robot.launch.py
@@ -1,10 +1,10 @@
-import os
-from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
-from launch.actions import GroupAction, IncludeLaunchDescription
+from launch import LaunchDescription
 from launch_ros.actions import PushRosNamespace
-from ament_index_python import get_package_share_directory
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from launch.actions import GroupAction, IncludeLaunchDescription
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 
 
 def generate_launch_description():
@@ -45,11 +45,17 @@ def generate_launch_description():
                 # push_ros_namespace to set namespace of included nodes
                 PushRosNamespace('robot_0'),
                 IncludeLaunchDescription(
-                    XMLLaunchDescriptionSource(
-                        os.path.join(
-                            get_package_share_directory('ros_sgp_tools'),
-                            'launch/mavros.launch')),
-                ),
+                    XMLLaunchDescriptionSource([
+                        PathJoinSubstitution([
+                            FindPackageShare('ros_sgp_tools'),
+                            'launch',
+                            'mavros.launch'
+                        ])
+                    ]),
+                    launch_arguments={
+                        'fcu_url': 'udp://0.0.0.0:14550@'
+                    }.items()
+                )
             ]
         )
     ])
