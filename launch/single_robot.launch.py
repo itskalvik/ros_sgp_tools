5dd4b01db088f6fad6d7faf2d99f2b75ddcb1903
itskalvik <kjakkala@uncc.edu>
2024-07-13 10:34:54 -0400

    Clean python launch file

--- launch/single_robot.launch.py
+++ launch/single_robot.launch.py
@@ -8,6 +8,8 @@ from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 
 
 def generate_launch_description():
+    namespace = 'robot_0'
+    
     return LaunchDescription([
         Node(
             package='ros_sgp_tools',
@@ -22,28 +24,28 @@ def generate_launch_description():
         Node(
             package='ros_sgp_tools',
             executable='online_ipp.py',
-            namespace='robot_0',
+            namespace=namespace,
             name='online_ipp'
         ),
 
         Node(
             package='ros_sgp_tools',
             executable='depth_publisher.py',
-            namespace='robot_0',
+            namespace=namespace,
             name='depth_publisher'
         ),
 
         Node(
             package='ros_sgp_tools',
             executable='ipp_mission.py',
-            namespace='robot_0',
+            namespace=namespace,
             name='ipp_mission'
         ),
 
         GroupAction(
             actions=[
                 # push_ros_namespace to set namespace of included nodes
-                PushRosNamespace('robot_0'),
+                PushRosNamespace(namespace),
                 IncludeLaunchDescription(
                     XMLLaunchDescriptionSource([
                         PathJoinSubstitution([
