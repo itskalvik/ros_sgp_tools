Fix scaling issue in csv waypoint dumps

scripts/offline_ipp.py
@@ -192,11 +192,13 @@ class offlineIPP(Node):
                      label='Path', zorder=1, marker='o', c='r')
             plt.scatter(self.data[i][:, 1], self.data[i][:, 0],
                         s=1, label='Candidates', zorder=0)
-            np.savetxt(f'OfflineIPP-{i}.csv', path, delimiter=',')
+            np.savetxt(f'IPPMission-(-1)-{i}.csv', 
+                       self.X_scaler.inverse_transform(np.array(path)),
+                       delimiter=',')
         plt.scatter(self.home_position[:, 1], self.home_position[:, 0],
                     label='Home position', zorder=2, c='g')
         plt.legend()
-        plt.savefig(f'OfflineIPP.png')
+        plt.savefig(f'IPPMission-(-1)-{i}.png')
 
     '''
     Send the new waypoints to the trajectory planner and 

scripts/online_ipp.py
@@ -233,8 +233,10 @@ class OnlineIPP(Node):
                     marker='.', s=1)
         plt.plot(waypoints[:, 1], waypoints[:, 0], 
                  label='Path', marker='o', c='r')
-        plt.savefig(f'IPPMission-({self.current_waypoint}).png')
-        np.savetxt(f'IPPMission-({self.current_waypoint}).csv', waypoints)
+        plt.savefig(f'IPPMission-({self.current_waypoint+1}).png')
+        np.savetxt(f'IPPMission-({self.current_waypoint+1}).csv', 
+                   self.X_scaler.inverse_transform(waypoints),
+                   delimiter=',')
 
     def update_with_data(self, force_update=False):
         # Update the parameters and waypoints if the buffer is full and 