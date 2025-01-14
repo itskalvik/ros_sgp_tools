5809e80fd79790f5335940204c5d342c022974e5
itskalvik <itskalvik@gmail.com>
2025-01-14 10:45:34 -0600

    Fix launch file

--- launch/single_robot.launch.py
+++ launch/single_robot.launch.py
@@ -84,66 +84,15 @@ def generate_launch_description():
 
     mission_planner = Node(package='ros_sgp_tools',
                            executable='ipp_mission.py',
-                           namespace=namespace,
                            name='IPPMission')
     nodes.append(mission_planner)
 
-    mavros = GroupAction(
-                    actions=[
-                        # push_ros_namespace to set namespace of included nodes
-                        PushRosNamespace(namespace),
-                        # MAVROS
-                        IncludeLaunchDescription(
-                            XMLLaunchDescriptionSource([
-                                PathJoinSubstitution([
-                                    FindPackageShare('ros_sgp_tools'),
-                                    'launch',
-                                    'mavros.launch'
-                                ])
-                            ]),
-                            launch_arguments={
-                            }.items()
-                        ),
-                    ]
-                )
-    nodes.append(mavros)
-
-    if start_foxglove:
-        foxglove = GroupAction(
-                        actions=[
-                            # push_ros_namespace to set namespace of included nodes
-                            PushRosNamespace(namespace),
-                            # Foxglove (web-based rviz)
-                            IncludeLaunchDescription(
-                                XMLLaunchDescriptionSource([
-                                    PathJoinSubstitution([
-                                        FindPackageShare('foxglove_bridge'),
-                                        'launch',
-                                        'foxglove_bridge_launch.xml'
-                                    ])
-                                ]),
-                            )
-                        ]
-                    )
-        nodes.append(foxglove)
-   
     if fake_data and data_type=='SerialPing2':
         print("Publishing Fake Sonar Data")
         sensor = Node(package='ros_sgp_tools',
                       executable='lake_depth_publisher.py',
                       name='FakeSonarData',
                       namespace=namespace)
-        nodes.append(sensor)
-
-    if data_type=='Ping2':
-        ping2_port = get_var('PING2_PORT', '/dev/ttyUSB0')
-        sensor = Node(package='ping_sonar_ros',
-                      executable='ping1d_node',
-                      name='Ping2',
-                      namespace=namespace,
-                      parameters=[
-                        {'port': ping2_port}
-                      ])
-        nodes.append(sensor)       
+        nodes.append(sensor)  
 
     return LaunchDescription(nodes)
